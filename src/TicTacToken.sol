447e4dc5b3c90891f5b75e932e7da99f3086fbec
Connor Mendenhall <cmendenhall@8thlight.com>
2021-11-10 13:51:27 -0500

    Week 2 AM session

--- src/TicTacToken.sol
+++ src/TicTacToken.sol
@@ -2,27 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract TicTacToken {
-    string[9] public board;
 
-    function getBoard() public view returns (string[9] memory) {
+    uint256 public constant X = 1;
+    uint256 public constant O = 2;
+    uint256 public constant EMPTY = 0;
+    
+    uint256[9] board;
+
+    function getBoard() public returns (uint256[9] memory) {
         return board;
     }
 
-    function markSpace(uint256 i, string calldata symbol) public {
-        require(_validSymbol(symbol), "Invalid symbol");
-        require(_emptySpace(i), "Already marked");
-        board[i] = symbol;
+    function markBoard(uint index, uint mark) public {
+        board[index] = mark;
     }
 
-    function _emptySpace(uint256 i) internal view returns (bool) {
-        return _compareString(board[i], "");
-    }
-
-    function _validSymbol(string memory symbol) internal pure returns (bool) {
-        return _compareString(symbol, "X") || _compareString(symbol, "O");
-    }
-
-    function _compareString(string memory a, string memory b) internal pure returns (bool) {
-        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
-    }
 }
--- src/test/TicTacToken.t.sol
+++ src/test/TicTacToken.t.sol
@@ -4,36 +4,43 @@ pragma solidity ^0.8.0;
 import "./utils/TicTacTokenTest.sol";
 
 contract TestTTT is TicTacTokenTest {
-    function test_has_empty_board() public {
-        for (uint256 i=0; i<9; i++) {
-            assertEq(ttt.board(i), "");
+
+    uint256 X;
+    uint256 O;
+
+    function setUp() public override {
+        super.setUp();
+        X = ttt.X();
+        O = ttt.O();
+    }
+
+    function test_create_board_of_9() public {
+        uint256[9] memory board = ttt.getBoard();
+        assertEq(board.length, 9);
+    }
+
+    function test_empty_array_index() public {
+        uint256[9] memory board = ttt.getBoard();
+        assertEq(board[3], 0);
+    }
+
+    function test_empty_board() public {
+        uint256[9] memory emptyBoard;
+        assertTrue(assertBoardsEq(ttt.getBoard(), emptyBoard));
+    }
+
+    function test_marks_board() public {
+        ttt.markBoard(0, X);
+        assertEq(ttt.getBoard()[0], X);
+    }
+
+    function assertBoardsEq(uint[9] memory array1, uint[9] memory array2) private pure returns (bool) {
+        for (uint256 index = 0; index < array2.length; index++) {
+            if (array2[index] != array1[index]) {
+                return false;
+            }
         }
+        return true;
     }
 
-    function test_get_board() public {
-        string[9] memory expected = ["", "", "", "", "", "", "", "", ""];
-        string[9] memory actual = ttt.getBoard();
-        for (uint256 i=0; i<9; i++) {
-            assertEq(actual[i], expected[i]);
-        }
-    }
-
-    function test_can_mark_space_with_X() public {
-        ttt.markSpace(0, "X");
-        assertEq(ttt.board(0), "X");
-    }
-
-    function test_can_mark_space_with_O() public {
-        ttt.markSpace(0, "O");
-        assertEq(ttt.board(0), "O");
-    }
-
-    function testFail_cannot_mark_space_with_Z() public {
-        ttt.markSpace(0, "Z");
-    }
-
-    function testFail_cannot_overwrite_marked_space() public {
-        ttt.markSpace(0, "X");
-        ttt.markSpace(0, "O");
-    }
 }
