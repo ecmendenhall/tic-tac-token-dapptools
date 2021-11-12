ea877b509b74c68bea44c30f70704c9326d89a2b
Connor Mendenhall <cmendenhall@8thlight.com>
2021-11-12 16:22:14 -0500

    Additional validations, check for wins

--- src/TicTacToken.sol
+++ src/TicTacToken.sol
@@ -3,15 +3,64 @@ pragma solidity ^0.8.0;
 
 contract TicTacToken {
     uint256[9] public board;
-    uint256 public X = 1;
-    uint256 public O = 2;
-    
+
+    uint256 public constant X = 1;
+    uint256 public constant O = 2;
+    uint256 public constant EMPTY = 0;
+
+    uint256 public currentTurn = X;
+
+    uint256[3][8] public winningSequences = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
     function getBoard() public view returns (uint256[9] memory) {
         return board;
     }
 
-    function set(uint256 index, uint256 val) public {
-        require(val == X || val == O, "Invalid mark");
-        board[index] = val;
+    function set(uint256 space, uint256 piece) public {
+        require(space < 9, "Invalid space");
+        require(piece == X || piece == O, "Invalid piece");
+        require(board[space] == EMPTY, "Space is taken!");
+        require(piece == currentTurn, "It's not your turn!");
+        board[space] = piece;
+        _toggleTurn();
+    }
+
+    function _toggleTurn() private {
+        currentTurn = (currentTurn == X) ? O : X;
+    }
+
+    function winner() public view returns (uint256) {
+        for (uint256 i = 0; i < winningSequences.length; i++) {
+            uint256 check = _checkSequence(winningSequences[i]);
+            if (check != EMPTY) {
+                return check;
+            }
+        }
+        return EMPTY;
+    }
+
+    function _checkSequence(uint256[3] memory sequence)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 product = board[sequence[0]] *
+            board[sequence[1]] *
+            board[sequence[2]];
+        if (product == 1) {
+            return X;
+        } else if (product == 8) {
+            return O;
+        }
+        return EMPTY;
     }
 }
--- src/test/TicTacToken.t.sol
+++ src/test/TicTacToken.t.sol
@@ -17,31 +17,85 @@ contract TestTTT is TicTacTokenTest {
         uint256[9] memory expected_board = [uint256(0), 0, 0, 0, 0, 0, 0, 0, 0];
         uint256[9] memory actual_board = ttt.getBoard();
 
-        for(uint256 i=0; i < 9; i++) {
-           assertEq(actual_board[i], expected_board[i]);
+        for (uint256 i = 0; i < 9; i++) {
+            assertEq(actual_board[i], expected_board[i]);
         }
     }
 
     function test_set_board_with_X() public {
-        uint256 spot = 0;
+        ttt.set(0, X);
 
-        ttt.set(spot, X);
-
-        assertEq(ttt.board(spot), X);
+        assertEq(ttt.board(0), X);
     }
 
     function test_set_board_with_O() public {
-        uint256 spot = 4;
+        ttt.set(1, X);
+        ttt.set(4, O);
 
-        ttt.set(spot, O);
-        
-        assertEq(ttt.board(spot), O);
+        assertEq(ttt.board(4), O);
     }
 
     function testFail_set_board_with_other_mark() public {
-      uint256 spot = 1;
-
-      ttt.set(spot, 5);
+        ttt.set(1, 5);
     }
 
+    function testFail_space_must_be_valid() public {
+        ttt.set(9, X);
+    }
+
+    function testFail_space_must_be_empty_to_mark() public {
+        ttt.set(3, X);
+        ttt.set(3, O);
+    }
+
+    function test_gets_current_turn() public {
+        assertEq(ttt.currentTurn(), X);
+        ttt.set(3, X);
+        assertEq(ttt.currentTurn(), O);
+        ttt.set(1, O);
+        assertEq(ttt.currentTurn(), X);
+    }
+
+    function testFail_same_player_cannot_mark_twice_in_a_row() public {
+        ttt.set(3, X);
+        ttt.set(4, X);
+    }
+
+    function test_returns_winner_from_matching_row() public {
+        ttt.set(0, X);
+        ttt.set(3, O);
+        ttt.set(1, X);
+        ttt.set(4, O);
+        ttt.set(2, X);
+        assertEq(ttt.winner(), X);
+    }
+
+    function test_returns_winner_from_matching_column() public {
+        ttt.set(7, X);
+        ttt.set(0, O);
+        ttt.set(2, X);
+        ttt.set(3, O);
+        ttt.set(8, X);
+        ttt.set(6, O);
+        assertEq(ttt.winner(), O);
+    }
+
+    function test_returns_winner_from_matching_diagonal() public {
+        ttt.set(3, X);
+        ttt.set(0, O);
+        ttt.set(1, X);
+        ttt.set(4, O);
+        ttt.set(2, X);
+        ttt.set(8, O);
+        assertEq(ttt.winner(), O);
+    }
+
+    function test_returns_winner_from_matching_antidiagonal() public {
+        ttt.set(2, X);
+        ttt.set(0, O);
+        ttt.set(4, X);
+        ttt.set(1, O);
+        ttt.set(6, X);
+        assertEq(ttt.winner(), X);
+    }
 }
